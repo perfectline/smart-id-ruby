25b3f6874ba86e6c95075e3b4775a7e66570a634
Thales Ribeiro <thalessr@gmail.com>
2023-03-01 14:42:28 -0300

    Add rubocop and changes authentication to use API version 2

--- lib/smart_id/api/authentication/identity_number.rb
+++ lib/smart_id/api/authentication/identity_number.rb
@@ -1,28 +1,32 @@
-require "smart_id/api/authentication/base"
-require "smart_id/exceptions"
+# frozen_string_literal: true
 
-module SmartId::Api
-  module Authentication
-    class IdentityNumber < Base
-      BASE_URI = "authentication/pno"
+require 'smart_id/api/authentication/base'
+require 'smart_id/exceptions'
 
-      # @param country: 2 character ISO 3166-1 alpha-2 format(for example EE, LT, LV, KZ)
-      # @param identity_number: national identity number of the individuals
-      def initialize(**opts)
-        @country = opts[:country].upcase
-        @identity_number = opts[:identity_number]
+module SmartId
+  module Api
+    module Authentication
+      class IdentityNumber < Base
 
-        unless @country && @identity_number
-          raise InvalidParamsError
+        BASE_URI = 'authentication/etsi'
+
+        # @param country: 2 character ISO 3166-1 alpha-2 format(for example EE, LT, LV, KZ)
+        # @param identity_number: national identity number of the individuals
+        def initialize(**opts)
+          @country = opts[:country].upcase
+          @identity_number = opts[:identity_number]
+
+          raise InvalidParamsError unless @country && @identity_number
+
+          super(**opts)
         end
 
-        super(**opts)
-      end
+        private
 
-      private
+        def api_uri
+          "#{BASE_URI}/PNO#{@country}-#{@identity_number}"
+        end
 
-      def api_uri
-        "#{BASE_URI}/#{@country}/#{@identity_number}"
       end
     end
   end
